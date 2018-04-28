53f66aef0f41837100f33e4688fe561c0d7e7a16
Renato Cantarino <renatocantarino@outlook.com>
2018-04-28 17:42:18 -0300

    first commit

---
+++ src/main/java/com/example/demo/AppSpringApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppSpringApplication.class, args);
+	}
+}
---
+++ src/main/java/com/example/demo/Entidades/Usuario.java
@@ -0,0 +1,64 @@
+package com.example.demo.Entidades;
+
+import java.util.List;
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="USUARIO")
+public class Usuario {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column
+	private String name;
+	
+	@Column
+	private String email;
+	
+	@ManyToMany
+	private Set<Perfil> roles;
+	
+	
+
+
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	
+	
+	
+	
+
+}
